bf010760fb308073824a3b4fca7b67b89b5675a0
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-11-04 18:00:38 -0600

    # fix front

---
+++ src/@components/Global/Spinner/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './index.scss';
+
+const Spinner = () => {
+  return <div className={classes.loaderAni}>Loading...</div>;
+};
+
+export default Spinner;
